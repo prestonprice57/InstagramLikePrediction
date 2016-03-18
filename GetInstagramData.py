13bf698bd37a91d60471a18734be4926c85dca8e
Fed <fedffm@gmail.com>
2016-03-18 16:34:51 -0600

    Added some comments and added if/else statement to help test different classifiers

--- GetInstagramData.py
+++ GetInstagramData.py
@@ -1,11 +1,9 @@
-from instagram.client import InstagramAPI
+import json
+import requests
 import indicoio
-from collections import Counter
-from facepp import API as face_api
 import numpy as np
-import calendar
-import json, requests
-import pprint
+from facepp import API as face_api
+from instagram.client import InstagramAPI
 
 client_id = "bec82b4b69cc435998eb2c9f82212fb4"
 client_secret = "6f7cd017a78945afaffcd992840a8fe5"
@@ -24,80 +22,75 @@ INPUT_FILE = 'InstagramUsersx100000.txt'
 users = []
 f = open(INPUT_FILE, 'r')
 for line in f:
-	new_line, end_line = line.split('\n')
-	users.append(new_line)
+    new_line, end_line = line.split('\n')
+    users.append(new_line)
 f.close()
 
 np.random.seed(1024)
 indices = np.random.permutation(len(users))
 
-
 startIndex = 0
 endIndex = 10
-while endIndex < len(indices): #CHANGE THIS
-	jsonFile = {}
-	with open('data.json') as infile:
-		jsonFile = json.load(infile)
+while endIndex < len(indices):  # CHANGE THIS
+    jsonFile = {}
+    with open('data.json') as infile:
+        jsonFile = json.load(infile)
 
-	for i in xrange(startIndex,endIndex):
-		new_user = users[indices[i]]
-		print new_user
-		isPrivate = api.user_relationship(user_id=new_user).target_user_is_private
-		if isPrivate == False:
+    for i in range(startIndex, endIndex):
+        new_user = users[indices[i]]
+        print(new_user)
+        isPrivate = api.user_relationship(user_id=new_user).target_user_is_private
+        if isPrivate == False:
 
-			url = 'https://api.instagram.com/v1/users/self?access_token=%s' % access_token
-			resp = requests.get(url=url)
-			data = resp.json()
-			follows = data['data']['counts']['follows']
-			followers = data['data']['counts']['followed_by']
+            url = 'https://api.instagram.com/v1/users/self?access_token=%s' % access_token
+            resp = requests.get(url=url)
+            data = resp.json()
+            follows = data['data']['counts']['follows']
+            followers = data['data']['counts']['followed_by']
 
+            recent_media, next = api.user_recent_media(user_id=new_user, count=4)
+            for media in recent_media:
+                image_url = media.get_standard_resolution_url()
 
-			recent_media, next = api.user_recent_media(user_id=new_user, count=4)
-			for media in recent_media:
-				image_url = media.get_standard_resolution_url()
-				
-				if  media.type != 'video' and 1 <= len(face.detection.detect(url=image_url)['face']) <= 4:
-					day = media.created_time.weekday()
-					hour = str(media.created_time.hour) + ':' + str(media.created_time.minute)
-					likes = media.like_count
-					hashtags = len(media.tags)
+                if media.type != 'video' and 1 <= len(face.detection.detect(url=image_url)['face']) <= 4:
+                    day = media.created_time.weekday()
+                    hour = str(media.created_time.hour) + ':' + str(media.created_time.minute)
+                    likes = media.like_count
+                    hashtags = len(media.tags)
 
-					captionSentiment = 0.5
-					if media.caption != None:
-						caption = media.caption.text.replace('\n', ' ').replace('\r', ' ').encode('utf-8')
-						captionSentiment = indicoio.sentiment(caption)
+                    captionSentiment = 0.5
+                    if media.caption != None:
+                        caption = media.caption.text.replace('\n', ' ').replace('\r', ' ').encode('utf-8')
+                        captionSentiment = indicoio.sentiment(caption)
 
-					fer = indicoio.fer(image_url)
-					 
-					new_post = {}
-					new_post['happy'] = fer['Happy']
-					new_post['sad'] = fer['Sad']
-					new_post['angry'] = fer['Angry']
-					new_post['fear'] = fer['Fear']
-					new_post['surprise'] = fer['Surprise']
-					new_post['neutral'] = fer['Neutral']
-					new_post['day'] = day
-					new_post['hour'] = hour
-					new_post['likes'] = likes 
-					new_post['follows'] = follows
-					new_post['followers'] = followers
-					new_post['hashtags'] = hashtags
-					new_post['captionSentiment'] = captionSentiment
-					new_post['likeRatio'] = float(likes)/followers
-					new_post['followerRatio'] = float(followers)/follows
-					jsonFile['data']['posts'].append(new_post)
+                    fer = indicoio.fer(image_url)
 
-	f5 = open('NumPosts.txt', 'a')
-	f5.write("%s\n" % endIndex)
-	f5.close()
+                    new_post = {}
+                    new_post['happy'] = fer['Happy']
+                    new_post['sad'] = fer['Sad']
+                    new_post['angry'] = fer['Angry']
+                    new_post['fear'] = fer['Fear']
+                    new_post['surprise'] = fer['Surprise']
+                    new_post['neutral'] = fer['Neutral']
+                    new_post['day'] = day
+                    new_post['hour'] = hour
+                    new_post['likes'] = likes
+                    new_post['follows'] = follows
+                    new_post['followers'] = followers
+                    new_post['hashtags'] = hashtags
+                    new_post['captionSentiment'] = captionSentiment
+                    new_post['likeRatio'] = float(likes) / followers
+                    new_post['followerRatio'] = float(followers) / follows
+                    jsonFile['data']['posts'].append(new_post)
 
-	print endIndex
+    f5 = open('NumPosts.txt', 'a')
+    f5.write("%s\n" % endIndex)
+    f5.close()
 
-	startIndex += 10
-	endIndex += 10
+    print(endIndex)
 
+    startIndex += 10
+    endIndex += 10
 
-	with open('data.json', 'w') as outfile:
-		json.dump(jsonFile, outfile, sort_keys=True,indent=4)
-
-
+    with open('data.json', 'w') as outfile:
+        json.dump(jsonFile, outfile, sort_keys=True, indent=4)
--- Predict.py
+++ Predict.py
@@ -8,73 +8,123 @@ import numpy as np
 json_file = open('data.json')
 json_str = json_file.read()
 json_dict = json.loads(json_str)
-
 print type(json_dict)
 
+
+#
+#
+# Save all of the like ratios as targets
 target = []
 for d in json_dict['data']['posts']:
-	target.append(d['likeRatio'])
+    print d['likeRatio']
+    target.append(d['likeRatio'])
 
 
+#
+#
+# Now remove the like ratios AND the likes (so that we can predict)
 for d in json_dict['data']['posts']:
-	hour, minute = d['hour'].split(':')
-	d['hour'] = float(hour+minute)
-	del d['likeRatio']
-	del d['likes']
+    hour, minute = d['hour'].split(':')
+    d['hour'] = float(hour + minute)
+    del d['likeRatio']
+    del d['likes']
 data = json_dict['data']['posts']
 
 
+#
+#
+# Copy the data
 x = []
 for d in data:
-	temp = []
-	for key,val in d.iteritems():
-		temp.append(val)
-	x.append(temp)
+    temp = []
+    for key, val in d.iteritems():
+        temp.append(val)
+    x.append(temp)
 
-x_train = x[0:int(len(x)*0.9)]
-x_test = x[int(len(x)*0.9):]
-target_train = target[0:int(len(target)*0.9)]
-target_test = target[int(len(target)*0.9):]
 
-#for val in x_test:
-#	val[8] = 0
-'''
-clf = DecisionTreeRegressor(max_depth=20)
-clf = clf.fit(x_train, target_train)
+#
+#
+# Prepare the training and testing sets
+x_train = x[0:int(len(x) * 0.9)]
+x_test = x[int(len(x) * 0.9):]
+target_train = target[0:int(len(target) * 0.9)]
+target_test = target[int(len(target) * 0.9):]
 
-predict = clf.predict(x_test)
-'''
-sv = svm.SVR(kernel='poly')
-sv = sv.fit(x_train, target_train)
-predict = sv.predict(x_test)
-'''
 
+#
+#
+# Decision Tree
+# clf = DecisionTreeRegressor(max_depth=20)
+# clf = clf.fit(x_train, target_train)
+# clf.predict(x_test)    # TODO: This classifier has no .predict method
+
+
+# SVM
+# sv = svm.SVR(kernel='poly')
+# sv = sv.fit(x_train, target_train)   # TODO: ONE OF THESE LINES CAUSES ISSUES
+# predict = sv.predict(x_test)         # TODO: ONE OF THESE LINES CAUSES ISSUES
+
+
+#
+#
+# Naive Bayes
+# The predicted like ratios, on average, are usually a lot higher than the actual ratios
+# Example: predicted: 12.3, actual: 1.6
 bayes = linear_model.BayesianRidge()
 bayes = bayes.fit(x_train, target_train)
-predict = bayes.predict(x_test)
 
+
+#
+#
+# K Nearest Neighbors
+# More accurate than Naive Bayes
+# There are more predicted ratios that fall into the correct range, but there are much crazier outliers!
+# Example: predicted: 51.3, actual: 5.3
 knn = neighbors.KNeighborsRegressor()
 knn = knn.fit(x_train, target_train)
-predict = knn.predict(x_test)
 
 
+#
+#
+# Linear Model
 lr = linear_model.LinearRegression(normalize=True)
 lr = lr.fit(x_train, target_train)
-predict = lr.predict(x_test)
-'''
 
+
+#
+#
+# Which model do we want to test?
+kNN = False
+naive_bayes = True
+linear = False
+decisionTree = False
+
+if kNN:
+    predict = knn.predict(x_test)
+elif naive_bayes:
+    predict = bayes.predict(x_test)
+elif linear:
+    lr.predict(x_test)
+
+
+#
+#
+# Get the accuracy
 correct = 0
 wrong = 0
 for i, val in enumerate(predict):
-	print i, val, target_test[i]
-	accuracy = target_test[i]*1
-	if target_test[i]-accuracy <= val <= target_test[i]+accuracy:
-		correct+=1
-	else:
-		wrong+=1
+    print i, "predicted:", val, "actual:", target_test[i]
+    accuracy = target_test[i] * 1
+    if target_test[i] - accuracy <= val <= target_test[i] + accuracy:
+        correct += 1
+    else:
+        wrong += 1
 
+
+#
+#
+# Print results
+print '\n'
 print 'CORRECT:', correct
 print 'WRONG:', wrong
-print 'TOTAL ACCURACY:', float(correct)/len(predict)
-
-
+print 'TOTAL ACCURACY:', float(correct) / len(predict)
